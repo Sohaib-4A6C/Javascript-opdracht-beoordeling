e852f5899bb66ab94f980e7e848948668c3ff769
Sohaib-4A6C <513484@edu.rocmn.nl>
2017-02-09 11:27:48 +0100

    Alles geupdate nu opdracht is af"

--- beoordelingenlijst.js
+++ beoordelingenlijst.js
@@ -17,3 +17,23 @@ function myFunction() {
         document.getElementById('resultaat').innerHTML = 'Het cijfer is ' + cijfer + ' en is dus goed'
     }
 }
+
+function myFunction1() {
+    var cijfer = document.getElementById('cijfer1').value
+    
+    if (cijfer >=0 && cijfer <=50){
+        document.getElementById('resultaat').innerHTML = 'De beoordeling is ovoldoende want het cijfer is ' + cijfer
+    } 
+    
+    if  (cijfer >=49 && cijfer <=60){
+        document.getElementById('resultaat').innerHTML = 'De beoordeling is matig want het cijfer is ' + cijfer
+    } 
+    
+    if (cijfer >=59 && cijfer <=75){
+        document.getElementById('resultaat').innerHTML = 'De beoordeling is voldoende want het cijfer is ' + cijfer
+    }
+    
+    if (cijfer >=74 && cijfer <=101){
+        document.getElementById('resultaat').innerHTML = 'De beoordeling is goed want het cijfer is ' + cijfer
+    }
+}
